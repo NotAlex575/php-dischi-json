9675853dd3f973dd16e05085fedc78eb3ca02122
NotAlex575 <dapontealessandro89gmail.com>
2025-11-03 17:48:30 +0100

    Test per mostrare in index.php il contenuto dell'array di oggetti

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     $json_dischi = file_get_contents("dischi.json");
     $dischi = json_decode($json_dischi, true);
+    var_dump($dischi);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +17,15 @@
     <div>
         <ul>
             <?php
-                var_dump($dischi);
+                echo "____________________________________";
+                foreach($dischi as $disco){
+                    echo ':<li>Titolo: '.$disco["titolo"].'</li>';
+                    echo ':<li>Artista: '.$disco["artista"].'</li>';
+                    echo ':<li>URL Cover: '.$disco["url_della_cover"].'</li>';
+                    echo ':<li>Anno di pubblicazione: '.$disco["anno_di_pubblicazione"].'</li>';
+                    echo ':<li>Genere: '.$disco["genere"].'</li>';
+                    echo "____________________________________";
+                }
             ?>
         </ul>
     </div>
